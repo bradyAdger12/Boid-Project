d81e9c50d19431c505a89d6aa6cac6c5b86653e1
bradyAdger12 <35546510+bradyAdger12@users.noreply.github.com>
2018-11-05 13:05:55 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,43 @@
+# boids
+modeling group behavior using multiple independent agents.  Based on Craig W.
+Reynolds Flocks, Herds, and Schools: A Distributed Behavior Model",
+Computer Graphics 21:4, pp. 25-34
+
+
+# Group Assignments
+Configuration/User interface:
+
+* Denney, Isaac A
+* Droege, Jonathan Linclon
+* Gray, Anthony
+* Skirmants, Luke
+* Stratman, Dylan
+* Thibodeau, Jack
+* Trninich, Tyler James
+
+Boid/Flock/Behavior
+
+* Adger, Preston B
+* Barnard, Mick
+* Deleeuw, Brendan Aaron
+* Frentzel, Daniel Robert Thomas
+* Funes, Ryan Alexander
+* Hirschman, Joshua Evan Metzger
+* McCall, Kaiden Kwynn
+* Rijks, Diana
+* Smith, Caleb
+* Van Iwaarden, River
+* Borner, Matthew
+
+World Control/Rendering
+
+* Arkadie, Anthoney Bernett
+* Bontecou, Cody R.
+* Calhoun, Shane M.
+* Colton, Stephanie Michelle
+* Fry, Joshua
+* Hodson, Benjamin
+* Linscheid, Mica Eban
+* Montesinos, Esteban
+* Olmstead, Travis Everett
+* Welch, Dalton Ray
---
+++ src/P3.py
@@ -0,0 +1,43 @@
+import math
+
+
+class P3:
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __rmul__(self, scalar):
+        return P3(self.x * scalar, self.y * scalar, self.z * scalar)
+
+    def __mul__(self, other):
+        if type(other) == int or type(other) == float:
+            return self.__rmul__(other)
+        return P3(self.y*other.z - self.z*other.y, self.z*other.x - self.x*other.z, self.x*other.y - self.y*other.x)
+
+    def __add__(self, other):
+        return P3(self.x+other.x, self.y+other.y, self.z+other.z)
+
+    def distance(self, other=None):
+        other = other if not None else P3(0, 0, 0)
+        return math.sqrt((self.x+other.x)**2 + (self.y+other.y)**2 + (self.z+other.z)**2)
+
+    def normalize(self):
+        magnitude = self.distance()
+        return P3(self.x/magnitude, self.y/magnitude, self.z/magnitude)
+
+    def __str__(self):
+        return '(' + str(self.x) + ', ' + str(self.y) + ', ' + str(self.z) + ')'
+
+
+if __name__ == "__main__":
+    p1 = P3(1, 1, 1)
+    p2 = P3(2, 5, 10)
+
+
+
+    print(2 * p1)
+    print(p1 * 2)
+    print(p1 * p2)
+
